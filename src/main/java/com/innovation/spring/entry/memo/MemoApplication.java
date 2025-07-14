52a2214f1ac1d942abac46dd2aa85a3a6cfd3ea6
legoChoi <chlx159@gmail.com>
2025-07-14 14:57:03 +0900

    Initial commit

---
+++ src/main/java/com/innovation/spring/entry/memo/MemoApplication.java
@@ -0,0 +1,13 @@
+package com.innovation.spring.entry.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoApplication.class, args);
+	}
+
+}
