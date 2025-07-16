42aa00c51d20fefd098ad41c22247cd6245b9e61
legoChoi <chlx159@gmail.com>
2025-07-16 19:32:19 +0900

    :memo: docs: add README.md - level 2

--- src/main/java/com/innovation/spring/entry/memo/lv02/repository/MemoRepository.java
+++ src/main/java/com/innovation/spring/entry/memo/lv02/repository/MemoRepository.java
@@ -8,8 +8,18 @@ import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
+    /**
+     * JPQL을 사용한 조회
+     * @param keyword
+     * @return
+     */
     @Query("select m from Memo m where m.contents like concat('%', :keyword, '%') order by m.updatedAt desc")
     List<Memo> getMemosByKeyword(String keyword);
 
+    /**
+     * 쿼리 메소드를 사용한 조회
+     * @param keyword
+     * @return
+     */
     List<Memo> findMemosByContentsContainsOrderByUpdatedAtDesc(String keyword);
 }
